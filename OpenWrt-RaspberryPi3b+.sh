6c97c50775745e049fc9c5897fab2f71ce75b3c4
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-02-02 18:11:16 +0800

    Update OpenWrt-RaspberryPi3b+.sh

--- OpenWrt-RaspberryPi3b+.sh
+++ OpenWrt-RaspberryPi3b+.sh
@@ -27,6 +27,9 @@ git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
 #git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-Jo
 git clone https://github.com/Flowers-in-thorns/luci-app-vssr-coexist.git package/luci-app-vssr-coexist
 
+# uci-app-diskman
+ln -s package/luci-app-diskman/Parted.Makefile package/parted/Makefile
+
 
 
 #创建自定义配置文件 - OpenWrt-树莓派3b+
